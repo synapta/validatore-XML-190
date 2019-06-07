7b1c8342139d24aa71529314f5508632a59701d6
alemela <alessiom92@gmail.com>
2019-06-07 17:47:09 +0200

    dq functions draft

--- src/data-quality-functions.js
+++ src/data-quality-functions.js
@@ -1,3 +1,18 @@
+exports.validaData = function (data) {
+    let regex = /^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])$/g;
+    if (data.match(regex)) {
+        return true;
+    }
+    return false;
+}
+
+
 exports.dataFineDopoDataInizio = function (dataInizio, dataFine) {
-    
+    dataInizio = new Date(dataInizio);
+    dataFine = new Date(dataFine);
+
+    if (dataInizio <= dataFine) {
+        return true;
+    }
+    return false;
 }
