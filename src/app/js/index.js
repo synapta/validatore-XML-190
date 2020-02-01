55bcbefdbb13b435e9138c10f731379a91dc7ae4
Sofia Rosati <sofiarosati1@gmail.com>
2020-02-01 12:33:30 +0100

    add link to error from message

--- src/app/js/index.js
+++ src/app/js/index.js
@@ -1,20 +1,3 @@
-// $('#search-field').keypress(function (e) {
-//  var key = e.which;
-//  alert(e)
-//  if(key == 13) {
-//     alert('You pressed enter!');
-//   }
-// });
-
-// $('#load-file').click(function (e) {
-//  var key = e.which;
-//  alert(e)
-//  if(key == 13) {
-//     alert('You pressed enter!');
-//   }
-// });
-// onclick="window.location.href='/modifica'"
-
 // contratto singolo
 // http://www.comune.terni.it/amministrazione_trasparente/opendata/contratto/1313.xml
 // molti contratti
@@ -22,41 +5,39 @@
 // oggetti lunghi
 // http://hosting.soluzionipa.it/cardano_al_campo/benefici/appalti/esportazione_appalti_2019.xml
 
-//TODO loading page per aspettare la rispota dal sito, non solo per l'analisi!
 
 $('#load-site').click(function () {
+    $('#main').hide();
+    $('#loading').show();
     let url = $("#search-field").val();
     $.ajax({
         url: "/api/show/xml-from-site?url=" + encodeURI(url),
         type: 'GET',
         success: function(data) {
 
-        $('#main').hide();
-        $('#show').show();
-        $('#xml-form').text(data);
+            $('#loading').hide();
+            $('#show').show();
+            $('#xml-form').text(data);
 
-        window.myCodeMirror = CodeMirror.fromTextArea(document.getElementById("xml-form"), {
-            lineNumbers: true,
-            lineWrapping: true,
-            mode: 'xml'
-            // viewportMargin: Infinity --carica tutto il file, puoi fare il cerca, ma se è grosso danni
-        });
-        let xmlView = window.myCodeMirror;
-        data = formatData(xmlView, data);
-        // per marcare del testo
-        xmlView.markText({line: 50, ch: 0}, {line: 51, ch: 0}, {className: "styled-error" });
+            window.myCodeMirror = CodeMirror.fromTextArea(document.getElementById("xml-form"), {
+                lineNumbers: true,
+                lineWrapping: true,
+                mode: 'xml'
+                // viewportMargin: Infinity --carica tutto il file, puoi fare il cerca, ma se è grosso danni
+            });
+            let xmlView = window.myCodeMirror;
+            data = formatData(xmlView, data);
 
-        $('#a50').click( function(e) {
-            e.preventDefault();
-            xmlView.scrollIntoView({line:51, char:0})
-            return false;
-        });
-        $('#a20').click( function(e) {
-            e.preventDefault();
-            xmlView.scrollIntoView({line:21, char:0})
-            return false;
-        });
+            // per marcare del testo
+            markLine(xmlView,50,'error')
+            markLine(xmlView,20,'warning')
 
+            $(".error_line").click( function(e) {
+                let line = e.target.textContent;
+                e.preventDefault();
+                xmlView.scrollIntoView({line:line, char:0})
+                return false;
+            });
 
        }
     });
@@ -71,7 +52,9 @@ $('.message .close')
   })
 ;
 
-
+markLine = function (xmlView,line, type) {
+    xmlView.markText({line: line -1, ch: 0}, {line: line , ch: 0}, {className: "styled-"+type });
+}
 
 
 formatData = function (xmlView,data) {
