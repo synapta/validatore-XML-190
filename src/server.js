920a2aa6282880e6e5c926a32afcb50371e43312
sofiarosati <sofiarosati1@gmail.com>
2020-03-06 18:47:50 +0100

    bugfix payLoad too big

--- src/server.js
+++ src/server.js
@@ -4,7 +4,8 @@ var bodyParser   = require('body-parser');
 
 var app = express();
 //NEXT TWO LINES FOR READ BODY FROM POST
-app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.json({limit: '50mb', extended: true}));
+app.use(bodyParser.text({limit: '50mb', extended: true}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(morgan('common'));
 
